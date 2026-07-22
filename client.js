f58536118e63214e0bcce61694c31f4d3c5c282f
oda-source <oda-takashi65@hhms.co.jp>
2026-07-22 20:03:52 +0900

    Update client.js

--- client.js
+++ client.js
@@ -3,12 +3,12 @@ window.TrelloPowerUp.initialize({
   'board-buttons': function (t, opts) {
     return [{
       icon: 'https://api.iconify.design/ion:qr-code.svg?color=%234f46e5',
-      text: 'QR Flow Express',
+      text: 'QRコード依頼',
       callback: function (t) {
         return t.popup({
-          title: 'QR Flow Express',
+          title: 'QRコード依頼',
           url: './index.html',
-          height: 600
+          height: 680
         });
       }
     }];
