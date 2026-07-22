6554a879874e87c490203444cd1e7ba4b17adeff
oda-source <oda-takashi65@hhms.co.jp>
2026-07-22 21:24:24 +0900

    Update client.js

--- client.js
+++ client.js
@@ -5,34 +5,12 @@ window.TrelloPowerUp.initialize({
       icon: 'https://api.iconify.design/ion:qr-code.svg?color=%234f46e5',
       text: 'QRコード依頼',
       callback: function (t) {
-        return Promise.all([
-          t.board('url', 'name'),
-          t.cards('name', 'url')
-        ]).then(function (results) {
-          var board = results[0];
-          var cards = results[1] || [];
-
-          var targetCard = cards.find(function (card) {
-            return card && card.name && card.name.trim() === 'QR添付カード';
-          });
-
-          var targetUrl = targetCard ? targetCard.url : board.url;
-
+        return t.board('url', 'name').then(function (board) {
           return t.modal({
             title: 'QRコード依頼',
-            url: './index.html?targetUrl=' + encodeURIComponent(targetUrl) + '&boardName=' + encodeURIComponent(board.name),
+            url: './index.html?targetUrl=' + encodeURIComponent(board.url) + '&boardName=' + encodeURIComponent(board.name),
             height: 750,
             fullscreen: false
-          });
-        }).catch(function (err) {
-          console.error('Power-Up Error:', err);
-          return t.board('url', 'name').then(function (board) {
-            return t.modal({
-              title: 'QRコード依頼',
-              url: './index.html?targetUrl=' + encodeURIComponent(board.url) + '&boardName=' + encodeURIComponent(board.name),
-              height: 750,
-              fullscreen: false
-            });
           });
         });
       }
