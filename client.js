4c399cca4ca9d008539836736c85c598cdebe31e
oda-source <oda-takashi65@hhms.co.jp>
2026-07-22 20:40:23 +0900

    Update client.js

--- client.js
+++ client.js
@@ -5,10 +5,13 @@ window.TrelloPowerUp.initialize({
       icon: 'https://api.iconify.design/ion:qr-code.svg?color=%234f46e5',
       text: 'QRコード依頼',
       callback: function (t) {
-        return t.popup({
-          title: 'QRコード依頼',
-          url: './index.html',
-          height: 680
+        // 現在開いているボードの情報を取得してポップアップを開く
+        return t.board('url', 'name').then(function (board) {
+          return t.popup({
+            title: 'QRコード依頼',
+            url: './index.html?boardUrl=' + encodeURIComponent(board.url) + '&boardName=' + encodeURIComponent(board.name),
+            height: 680
+          });
         });
       }
     }];
