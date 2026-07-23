781d306c4883764a9b6f653c3b13ceb3592eca6c
oda-source <oda-takashi65@hhms.co.jp>
2026-07-23 11:28:22 +0900

    Update client.js

--- client.js
+++ client.js
@@ -6,8 +6,8 @@ window.TrelloPowerUp.initialize({
       text: 'QRコード依頼',
       callback: function (t) {
         return Promise.all([
-          t.board('url', 'name'),
-          t.cards('name', 'url')
+          t.board('url', 'name', 'id'),
+          t.cards('id', 'name', 'url', 'attachments')
         ]).then(function (results) {
           var board = results[0];
           var cards = results[1] || [];
@@ -17,10 +17,22 @@ window.TrelloPowerUp.initialize({
           });
 
           var targetUrl = targetCard ? targetCard.url : board.url;
+          
+          // 添付ファイル名をカンマ区切りで取得
+          var attachmentNames = [];
+          if (targetCard && targetCard.attachments) {
+            attachmentNames = targetCard.attachments.map(function (att) {
+              return att.name;
+            });
+          }
+
+          var modalUrl = './index.html?targetUrl=' + encodeURIComponent(targetUrl) +
+            '&boardName=' + encodeURIComponent(board.name) +
+            '&attachments=' + encodeURIComponent(JSON.stringify(attachmentNames));
 
           return t.modal({
             title: 'QRコード依頼',
-            url: './index.html?targetUrl=' + encodeURIComponent(targetUrl) + '&boardName=' + encodeURIComponent(board.name),
+            url: modalUrl,
             height: 750,
             fullscreen: false
           });
@@ -28,7 +40,7 @@ window.TrelloPowerUp.initialize({
           return t.board('url', 'name').then(function (board) {
             return t.modal({
               title: 'QRコード依頼',
-              url: './index.html?targetUrl=' + encodeURIComponent(board.url) + '&boardName=' + encodeURIComponent(board.name),
+              url: './index.html?targetUrl=' + encodeURIComponent(board.url) + '&boardName=' + encodeURIComponent(board.name) + '&attachments=%5B%5D',
               height: 750,
               fullscreen: false
             });
@@ -37,8 +49,7 @@ window.TrelloPowerUp.initialize({
       }
     }];
   },
-  
-  // ★ 2枚目の「Power-Upの設定を編集」をクリックした時の処理
+
   'show-settings': function (t, opts) {
     return t.popup({
       title: 'GAS連携設定',
